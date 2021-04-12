7087f853b1b30cce9271e812695f91cf3dac3b07
chaerin98 <47683552+chaerin98@users.noreply.github.com>
2021-04-12 22:25:44 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+let teapot;
+let rot;
+let slider;
+function preload() {
+teapot = loadModel('assets/teapot.obj');
 }
-
+function setup() {
+createCanvas(windowWidth, windowHeight, WEBGL);
+colorMode(HSB, 360, 100, 100);
+slider = createSlider(0, 360, 120);
+slider.position(10, 10);
+slider.style('width', '80px');
+rot=0;
+}
 function draw() {
-  background('FFFFFF');
-  box(305, 455, 200);
+background(0);
+// setting lights
+pointLight(0, 0, 100, 0, 0, 500);
+// transform the teapot
+translate(0, 200, -300);
+rotateX(HALF_PI);
+rotateZ(rot);
+scale(20);
+noStroke();
+// set teapot material colors
+fill(slider.value(), 100, 100);
+// display teapots
+model(teapot);
+// rotate teapots
+rot += 0.02;
 }
