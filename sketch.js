0e103c71b56e86437439556f83e06bd15dab70ae
chaerin98 <47683552+chaerin98@users.noreply.github.com>
2021-04-12 21:46:09 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let cola;
+let rot;
+
+function preload() {
+  cola = loadModel('assets/cola.stl');
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  colorMode(HSB, 360, 100, 100);
+  rot=0;
+}
+function draw() {
+  background(0);
+  // setting lights
+  pointLight(0, 0, 100, 0, 0, 500);
+  // transform the teapot
+  translate(0, 200, -300);
+  rotateX(HALF_PI);
+  rotateZ(rot);
+  scale(20);
+  noStroke();
+  // display teapots
+  model(cola);
+  // rotate teapots
+  rot += 0.02;
+}
